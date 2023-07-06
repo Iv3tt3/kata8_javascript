6ff7c373c91e1b0f1a3bde2963f329ba1e497785
Ivette Farre Prat <ivettefarreprat@Ivettes-MacBook-Pro.local>
2023-07-06 19:31:44 +0100

    Probatinas con JS

--- mi_cartera_js/static/js/app.js
+++ mi_cartera_js/static/js/app.js
@@ -1,21 +1,30 @@
-//alert("Hola mundo") Vamos a hacer que el JS no se renderice primero por defecto, sino cuando queramos renderizarlo
+var el_ultimo
 
-//creamos una funcion que va a ser llamada al pulsar el boton
 function pulsado(event){
-    event.preventDefault() // Le dice, no hagas lo que harias por defecto (es decir no lances la peticion GET POST del formulario html)
-    alert("Has hecho click")
+    event.preventDefault() 
+    let the_father = document.querySelector("#contenido")
+    let the_paragraph = document.createElemennt("p") 
+    
+    let input = document.querySelector("#texto")
+    the_paragraph.innerHTML = input.value 
+
+    the_father.appendChild(the_paragraph)
+
+    el_ultimo = the_paragraph
+}
+
+function borrado(event){
+    event.preventDefault() 
+    let the_father = document.querySelector("#contenido")
+    if (el_ultimo){
+        the_father.removeChild(el_ultimo)
+    } //En este if le decimos que solo elimino el ultimo si hay algo en la lista, si esta vacia no haga nada
 }
 
 window.onload = function() {
+    let boton = document.querySelector("#btnSubmit") 
+    boton.addEventListener("click", pulsado)
 
-//vamos a atrapar el evento de cuando pulse el boton crear me guarde el texto que introduzco en una variable
-    let boton = document.querySelector("#btnSubmit") //le pongo la id del boton que he puesto en index.html
-    alert(boton)
-//Al boton le pongo un metodo que escucha eventos
-    boton.addEventListener("click", pulsado)//Cuando escuches el evento click, vas a ejecutar la funcion pulsado. Buscar eventos de JS
-
+    let botonDelete = document.querySelector("#btnDelete") //Si no añadiesemos esto, llamaria el boton de la forma normal por HTML y te iria al servidor
+    botonDelete.addEventListener("click", borrado)
 }
-
-//IMPORTANTE QUE TODO ESTO SUCEDERA EN EL NAVEGADOR, No en el servidor.
-
-//Para que el evento click se gestione en el JS no en el HTML, es decir, al pulsar el boton clik, no haga un methodo GET o POST hacemos la function pulsado() -->LA METEMOS AL INICIO VER LINIA 4
